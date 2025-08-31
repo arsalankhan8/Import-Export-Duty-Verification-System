8456d650eb96bec6f9f46b98e4e156e5afa7d01b
Arsalan Khan <arsalankhan1102004@gmail.com>
2025-08-31 00:41:56 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // Ensure you're connecting to MongoDB Atlas
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
